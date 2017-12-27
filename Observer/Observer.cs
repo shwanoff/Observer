f2c5eacee64be2b397df2aeb5ef553e92827886b
Вадим Шванов <adm.shwan@gmail.com>
2017-12-27 18:06:14 +0300

    Add project files.

---
+++ Observer/Observer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Observer
+{
+    /// <summary>
+    /// Наблюдатель.
+    /// </summary>
+    public class Observer
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Создать экземпляр наблюдателя.
+        /// </summary>
+        /// <param name="title">Название наблюдателя.</param>
+        /// <param name="subject">Наблюдаемый объект.</param>
+        public Observer(string title, Subject subject)
+        {
+            Title = title;
+            subject.OnSaved += Subject_OnSaved;
+        }
+
+        /// <summary>
+        /// Обработчик события сохранения наблюдаемого объекта.
+        /// </summary>
+        /// <param name="sender">Наблюдаемый объект.</param>
+        /// <param name="e">Аргументы события.</param>
+        private void Subject_OnSaved(object sender, EventArgs e)
+        {
+            string format = "dd.MM.yyyy hh:mm:ss";
+            Console.WriteLine($"[{DateTime.Now.ToString(format)}] Наблюдатель '{this}': Выполнено сохранение наблюдаемого объекта '{sender}'");
+        }
+
+        /// <summary>
+        /// Приведение объекта к строке.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Observer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем наблюдаемый объект.
+            var subject = new Subject("Объект 1");
+
+            // Создаем наблюдателя.
+            var observer = new Observer("Наблюдатель 1", subject);
+
+            // Вызываем метод наблюдаемого объекта,
+            // который создает событие.
+            // Наблюдатель должен отреагировать на событие и
+            // вывести сообщение на консоль.
+            subject.Save();
+
+            // Остановимся, чтобы увидеть результат.
+            Console.ReadLine();
+        }
+    }
+}
